afc5b4167645cdc0f63522769ecc733a237bf169
Aleksandr Smirnov <genesboard@gmail.com>
2025-11-16 17:29:15 +0300

    add axios plugin

--- resources/js/plugins/index.ts
+++ resources/js/plugins/index.ts
@@ -1,9 +1,11 @@
+import axios from './axios'
 import pinia from './pinia'
 import vuetify from './vuetify'
 import type { App } from 'vue'
 
 export function registerPlugins(app: App) {
   app
+    .use(axios)
     .use(pinia)
     .use(vuetify)
 }
