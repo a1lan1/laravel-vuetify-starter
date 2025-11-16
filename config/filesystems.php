ab124b9be14c9cf6d99efabe1a65e4e673d54b21
Aleksandr Smirnov <genesboard@gmail.com>
2025-11-16 16:22:57 +0300

    add spatie/laravel-backup with schedule commands

--- config/filesystems.php
+++ config/filesystems.php
@@ -47,6 +47,13 @@ return [
             'report' => false,
         ],
 
+        'backups' => [
+            'driver' => 'local',
+            'root' => storage_path('app/backups'),
+            'serve' => true,
+            'throw' => false,
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,4 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
-
-Artisan::command('inspire', function (): void {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::command('backup:run')->daily()->at('08:00');
+Schedule::command('backup:clean')->daily()->at('20:00');
