5ce6bd74a7998f27405ac2b0613444b5883d0657
Aleksandr Smirnov <genesboard@gmail.com>
2025-11-18 12:19:31 +0300

    add user activity seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         if (! app()->environment('production')) {
             $this->call([
                 UserSeeder::class,
+                UserActivitySeeder::class,
             ]);
         }
     }
