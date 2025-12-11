dd21ccae96e2833c0e33ddae276b0794064b53e2
Aleksandr Smirnov <genesboard@gmail.com>
2025-12-11 10:21:25 +0300

    install api with sanctum

--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,9 @@
 use App\Http\Controllers\Api\UserActivityController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|-------------------------------------------------------------------------
-| API Routes
-|-------------------------------------------------------------------------
-| These routes are loaded by the framework within the "api" middleware
-| group and prefixed with /api. They are stateless and rate-limited
-| according to your application's configuration.
-*/
+Route::middleware('auth:sanctum')
+    ->get('user', fn (Request $request) => $request->user());
 
-Route::post('/user-activities', [UserActivityController::class, 'store'])
+Route::post('user-activities', [UserActivityController::class, 'store'])
     ->middleware('throttle:120,1')
     ->name('api.user-activities.store');
